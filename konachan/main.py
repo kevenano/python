10df97c358554d66e73e1e8ba3489fa196ffb322
kevenano <kevenano@outlook.com>
2020-05-14 19:50:00 +0800

    更新主程序，加强结构化 可从json直接下载 待测试

--- konachan/main.py
+++ konachan/main.py
@@ -45,7 +45,7 @@ typeDic = {
     "frames_string": "text",
     "frames": "text",
     "is_replaced": "tinyint(1)",
-    "is_dlFailed": "tinyint(1)"
+    "is_dlFailed": "tinyint(1)",
 }
 
 
@@ -164,8 +164,8 @@ class DB:
         return results
 
     def update(self, tableName, filed, value, whereClause):
-        sql = "update "+"`"+tableName+"`"+' '
-        sql = sql + "set "+"`"+filed+"`"+'=%s '
+        sql = "update " + "`" + tableName + "`" + " "
+        sql = sql + "set " + "`" + filed + "`" + "=%s "
         sql = sql + whereClause
         flag = self.execute(sql, value)
         return flag
@@ -204,9 +204,9 @@ def updateData(db, tableName, data):
                 work[k] = str(v)
             if typeDic[k] == "int" and v is None:
                 work[k] = -1
-            if k == 'id':
+            if k == "id":
                 continue
-            flag = db.update(tableName, k, work[k], r"where `id`="+str(work['id']))
+            flag = db.update(tableName, k, work[k], r"where `id`=" + str(work["id"]))
             if flag != 1:
                 failedList.append(work["id"])
                 break
@@ -215,44 +215,60 @@ def updateData(db, tableName, data):
 
 # 获取参数
 # mainParams: 所有参数 urlParams: 下载专用参数
+# 返回:
+# flag == 1 从本地json下载
+# flag == 0 从网站爬取json
 def getParams():
     mainParams = {}
     urlParams = {}
-    tags = input("Tags:\n")
-    startPage = int(input("Start Page:\n"))
-    maxPage = int(input("maxPage:\n"))
-    tableName = input("tableName:\n")
-    if startPage < 1:
-        startPage = 1
-    if maxPage < 1:
-        maxPage = 1
-    mainParams["startPage"] = startPage
-    mainParams["tags"] = tags
-    mainParams["maxPage"] = maxPage
-    mainParams["tableName"] = tableName
-    urlParams["page"] = startPage
-    urlParams["tags"] = tags
+    flag = 0
+    flag = int(input("Provide json file? 0 for NO, 1 for YES :\n"))
+    if flag != 0 and flag != 1:
+        print("Bye!")
+        exit()
+    if flag == 1:
+        jsonPath = input("Json file path:\n")
+        tableName = input("tableName:\n")
+        mainParams["jsonPath"] = jsonPath
+        mainParams["tableName"] = tableName
+    else:
+        tags = input("Tags:\n")
+        startPage = int(input("Start Page:\n"))
+        maxPage = int(input("maxPage:\n"))
+        tableName = input("tableName:\n")
+        if startPage < 1:
+            startPage = 1
+        if maxPage < 1:
+            maxPage = 1
+        mainParams["startPage"] = startPage
+        mainParams["tags"] = tags
+        mainParams["maxPage"] = maxPage
+        mainParams["tableName"] = tableName
+        urlParams["page"] = startPage
+        urlParams["tags"] = tags
 
-    return mainParams, urlParams
+    return mainParams, urlParams, flag
 
 
 # 具体下载函数.
 # data: 字典列表
-# sizeThr: 单文件大小阈值
 # starCnt: 初始计数
 # 返回:
 # data: 更新后的字典列表（包含下载状态信息）
 # endCnt: 结束时总图片计数
-def downPic(data, picPath, sizeThr=10 * 1024 * 1024, startCnt=0):
+def downPic(data, picPath, startCnt=0):
     picCnt = startCnt
     for item in data:
-        item['is_replaced'] = 0
-        item['is_dlFailed'] = 0
+        # 如果 is_replaced 不在字典中，说明传入数据为新抓取的数据
+        # 需要设置该字段
+        if "is_replaced" not in item:
+            item["is_replaced"] = 0
+            item["is_dlFailed"] = 0
         picCnt += 1
         pic_id = item["id"]
         pic_url = item["file_url"]
         pic_size = item["file_size"]
-        dtime = int(pic_size/(1024*1024)*60)
+        dtime = int(pic_size / (1024 * 1024) * 60)
         # 尝试下载源文件
         print("Picture count: ", picCnt, "ID: ", pic_id, "Size: ", pic_size)
         rawFile = download(url=pic_url, raw=1, timeout=(30, dtime))
@@ -260,17 +276,20 @@ def downPic(data, picPath, sizeThr=10 * 1024 * 1024, startCnt=0):
             # 替换下载jpeg文件
             pic_url = item["jpeg_url"]
             pic_size = item["jpeg_file_size"]
-            dtime = int(pic_size/(1024*1024)*60)
-            print('Try to download jpeg file instead... size: ', pic_size)
+            dtime = int(pic_size / (1024 * 1024) * 60)
+            print("Try to download jpeg file instead... size: ", pic_size)
             rawFile = download(url=pic_url, raw=1, timeout=(30, dtime))
             if rawFile is None:
                 # 下载失败
-                print('下载失败!')
-                item['is_dlFailed'] = 1
+                print("下载失败!")
+                # 更新状态标识
+                item["is_dlFailed"] = 1
             else:
                 # jpeg文件下载成功
-                print('下载成功!')
-                item['is_replaced'] = 1
+                print("下载成功!")
+                # 更新状态标识
+                item["is_dlFailed"] = 0
+                item["is_replaced"] = 1
                 # 保存于本地
                 tmpPath = os.path.join(picPath, str(pic_id) + pic_url[-4:])
                 picFile = open(tmpPath, "wb")
@@ -282,7 +301,10 @@ def downPic(data, picPath, sizeThr=10 * 1024 * 1024, startCnt=0):
                 print()
         else:
             # 源文件下载成功
-            print('下载成功!')
+            print("下载成功!")
+            # 更新状态标识
+            item["is_dlFailed"] = 0
+            item["is_replaced"] = 0
             # 保存于本地
             tmpPath = os.path.join(picPath, str(pic_id) + pic_url[-4:])
             picFile = open(tmpPath, "wb")
@@ -299,23 +321,84 @@ def downPic(data, picPath, sizeThr=10 * 1024 * 1024, startCnt=0):
     return data, endCnt
 
 
-# 主函数
-def main():
-    # 记录时间 创建工作目录
-    startTime = time.time()
-    taskId = int(startTime)
-    taskPath = os.path.join(os.getcwd(), str(taskId))
-    os.mkdir(taskPath)
-    jsonPath = os.path.join(taskPath, "json")
-    os.mkdir(jsonPath)
-    picPath = os.path.join(taskPath, "picture")
-    os.mkdir(picPath)
-    sumPath = os.path.join(taskPath, "log.txt")
+# 根据json直接下载
+# 输入:
+# db: 数据库
+# mainParams: 主参数字典
+# 返回
+# dlFailed
+# inFailed
+# upFailed
+# picCnt
+def reDownload(db, mainParams):
+    # 初始化相关参数
+    tableName = mainParams["tableName"]
+    jsonPath = mainParams["jsonPath"]
+    picPath = mainParams["picPath"]
+    reList = []
+    dlFailed = []
+    inFailed = []
+    upFailed = []
+    # 尝试打开json文件
+    try:
+        jsFile = open(jsonPath, "r")
+    except Exception as e:
+        print(e)
+        return -1
+    data = json.load(jsFile)
+    jsFile.close()
+    # 下载文件并更新数据状态标识
+    data, picCnt = downPic(data, picPath)
+    newData = []
+    upData = []
+    # 检查数据库，去重 item 字典 data 列表
+    for item in data:
+        workId = item["id"]
+        sql = "select * from `" + tableName + "` " + "where id=" + str(workId)
+        flag = db.execute(sql)
+        if flag != 1:
+            print(flag.args[0], flag.args[1])
+            exit()
+        resulte = db.fetchall()
+        if len(resulte) == 0:
+            newData.append(item)
+        else:
+            upData.append(item)
+    # 写入新数据
+    failedList, dupList = insertData(db, tableName, newData)
+    if len(failedList) > 0:
+        for item in failedList:
+            inFailed.append(item)
+    for item in newData:
+        if item["is_replaced"] == 1:
+            reList.append(item["id"])
+        if item["is_dlFailed"] == 1:
+            dlFailed.append(item["id"])
+    # 更新旧数据
+    if len(upData) > 0:
+        failedList = updateData(db, mainParams["tableName"], upData)
+        if len(failedList) > 0:
+            for item in failedList:
+                upFailed.append(item)
 
-    # 获取参数
-    mainParams, urlParams = getParams()
+    return reList, dlFailed, inFailed, upFailed, picCnt
 
+
+# 根据tags从网站爬取json
+# 输入:
+# db: 数据库
+# mainParams: 主参数字典
+# urlParams: 下载参数字典
+# 返回
+# dlFailed
+# inFailed
+# upFailed
+# picCnt
+# pageCnt
+def newDownload(db, mainParams, urlParams):
     # 初始化相关参数及列表
+    jsonPath = mainParams["jsonPath"]
+    picPath = mainParams["picPath"]
     url = "https://konachan.com/post.json"
     picCnt = 0
     pageCnt = 0
@@ -323,25 +406,7 @@ def main():
     dlFailed = []
     inFailed = []
     upFailed = []
-
-    # 链接数据库
-    db = DB("localhost", "root", "qo4hr[Pxm7W5", "konachan")
-    db.connect()
-
-    # 建表
-    flag = db.createTable(mainParams["tableName"], columns=typeDic, primaryKey="id")
-    if flag == 1:
-        print("建表成功!")
-    elif flag.args[0] == 1050:
-        print("表格已存在!")
-        print()
-    else:
-        print("建表失败!")
-        print()
-        print(flag.args[0], flag.args[1])
-        exit()
-
-    # 循环处理所有页面
+    # 循环爬取
     while True:
         # 下载并保存json
         if pageCnt == mainParams["maxPage"]:
@@ -389,13 +454,13 @@ def main():
                 for item in failedList:
                     inFailed.append(item)
             for item in newData:
-                if item['is_replaced'] == 1:
-                    reList.append(item['id'])
-                if item['is_dlFailed'] == 1:
-                    dlFailed.append(item['id'])
+                if item["is_replaced"] == 1:
+                    reList.append(item["id"])
+                if item["is_dlFailed"] == 1:
+                    dlFailed.append(item["id"])
         # 更新旧数据
         if len(upData) > 0:
-            failedList = updateData(db, mainParams['tableName'], upData)
+            failedList = updateData(db, mainParams["tableName"], upData)
             if len(failedList) > 0:
                 for item in failedList:
                     upFailed.append(item)
@@ -404,52 +469,140 @@ def main():
         urlParams["page"] += 1
         print()
 
-    # 断开数据库链接
-    db.close()
+    return reList, dlFailed, inFailed, upFailed, picCnt, pageCnt
 
-    # 写日志
+
+# 写日志
+def writeLog(
+    startTime,
+    logPath,
+    taskId,
+    mainParams,
+    pageCnt,
+    picCnt,
+    reList,
+    inFailed,
+    upFailed,
+    dlFailed,
+):
     endTime = time.time()
     spendTime = int(endTime - startTime)
-    sumFile = open(sumPath, "w")
-    sumFile.write("Task ID:\n")
-    sumFile.write(str(taskId) + "\n")
-    sumFile.write("Tags:\n")
-    sumFile.write(mainParams["tags"] + "\n")
-    sumFile.write("Pages:\n")
-    sumFile.write(
+    logFile = open(logPath, "w")
+    logFile.write("Task ID:\n")
+    logFile.write(str(taskId) + "\n")
+    logFile.write("Tags:\n")
+    logFile.write(mainParams["tags"] + "\n")
+    logFile.write("Pages:\n")
+    logFile.write(
         str(mainParams["startPage"])
         + "-"
         + str(mainParams["startPage"] + pageCnt - 1)
         + "\n"
     )
-    sumFile.write("New Work Count:\n")
-    sumFile.write(str(picCnt) + "\n")
-    sumFile.write("Replaced Count:\n")
-    sumFile.write(str(len(reList)) + "\n")
-    sumFile.write("Replaced List:\n")
-    sumFile.write(str(reList).replace(",", "\n") + "\n")
-    sumFile.write("Insert Failed Count:\n")
-    sumFile.write(str(len(inFailed)) + "\n")
-    sumFile.write("Insert Failed List:\n")
-    sumFile.write(str(inFailed).replace(",", "\n") + "\n")
-    sumFile.write("Update Failed Count:\n")
-    sumFile.write(str(len(upFailed)) + "\n")
-    sumFile.write("Update Failed List:\n")
-    sumFile.write(str(upFailed).replace(",", "\n") + "\n")
-    sumFile.write("Download Failed Count:\n")
-    sumFile.write(str(len(dlFailed)) + "\n")
-    sumFile.write("Download Failed List:\n")
-    sumFile.write(str(dlFailed).replace(",", "\n") + "\n")
-    sumFile.write("Start Time:\n")
-    sumFile.write(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(startTime)) + "\n")
-    sumFile.write("End Time:\n")
-    sumFile.write(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(endTime)) + "\n")
-    sumFile.write("Time Cost:\n")
-    sumFile.write(str(spendTime // 3600) + "h")
-    sumFile.write(str((spendTime % 3600) // 60) + "m")
-    sumFile.write(str(spendTime % 60) + "s" + "\n")
-    sumFile.close()
-    print("All finish!")
+    logFile.write("New Work Count:\n")
+    logFile.write(str(picCnt) + "\n")
+    logFile.write("Replaced Count:\n")
+    logFile.write(str(len(reList)) + "\n")
+    logFile.write("Replaced List:\n")
+    logFile.write(str(reList).replace(",", "\n") + "\n")
+    logFile.write("Insert Failed Count:\n")
+    logFile.write(str(len(inFailed)) + "\n")
+    logFile.write("Insert Failed List:\n")
+    logFile.write(str(inFailed).replace(",", "\n") + "\n")
+    logFile.write("Update Failed Count:\n")
+    logFile.write(str(len(upFailed)) + "\n")
+    logFile.write("Update Failed List:\n")
+    logFile.write(str(upFailed).replace(",", "\n") + "\n")
+    logFile.write("Download Failed Count:\n")
+    logFile.write(str(len(dlFailed)) + "\n")
+    logFile.write("Download Failed List:\n")
+    logFile.write(str(dlFailed).replace(",", "\n") + "\n")
+    logFile.write("Start Time:\n")
+    logFile.write(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(startTime)) + "\n")
+    logFile.write("End Time:\n")
+    logFile.write(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(endTime)) + "\n")
+    logFile.write("Time Cost:\n")
+    logFile.write(str(spendTime // 3600) + "h")
+    logFile.write(str((spendTime % 3600) // 60) + "m")
+    logFile.write(str(spendTime % 60) + "s" + "\n")
+    logFile.close()
+
+
+# 主函数
+def main():
+    # 记录时间 创建工作目录
+    startTime = time.time()
+    taskId = int(startTime)
+    taskPath = os.path.join(os.getcwd(), str(taskId))
+    os.mkdir(taskPath)
+    jsonPath = os.path.join(taskPath, "json")
+    os.mkdir(jsonPath)
+    picPath = os.path.join(taskPath, "picture")
+    os.mkdir(picPath)
+    logPath = os.path.join(taskPath, "log.txt")
+
+    # 获取参数
+    mainParams, urlParams, pflag = getParams()
+
+    # 初始化相关参数及列表
+    picCnt = 0
+    pageCnt = 0
+    reList = []
+    dlFailed = []
+    inFailed = []
+    upFailed = []
+
+    # 链接数据库
+    db = DB("localhost", "root", "qo4hr[Pxm7W5", "konachan")
+    db.connect()
+
+    # 建表
+    flag = db.createTable(mainParams["tableName"], columns=typeDic, primaryKey="id")
+    if flag == 1:
+        print("建表成功!")
+    elif flag.args[0] == 1050:
+        print("表格已存在!")
+        print()
+    else:
+        print("建表失败!")
+        print()
+        print(flag.args[0], flag.args[1])
+        exit()
+
+    # 直接从json文件下载
+    if pflag == 1:
+        mainParams["picPath"]
+        reList, dlFailed, inFailed, upFailed, picCnt = reDownload(db, mainParams)
+        pageCnt = 1
+
+    # 从网站爬取
+    if pflag == 0:
+        mainParams["picPath"]
+        mainParams["jsonPath"] = jsonPath
+        mainParams["picPath"] = picPath
+        reList, dlFailed, inFailed, upFailed, picCnt, pageCnt = newDownload(
+            db, mainParams, urlParams
+        )
+
+    # 断开数据库链接
+    db.close()
+
+    # 写日志
+    writeLog(
+        startTime,
+        logPath,
+        taskId,
+        mainParams,
+        pageCnt,
+        picCnt,
+        reList,
+        inFailed,
+        upFailed,
+        dlFailed,
+    )
+
+    # 结束
+    print("\nAll finish!")
 
 
 if __name__ == "__main__":
